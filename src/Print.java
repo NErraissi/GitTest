42e98838e225e8705253abc1b0e7e775b71e4c65
NErraissi <nouredine863@revature.net>
2022-05-10 17:21:30 -0400

    git test

---
+++ src/Print.java
@@ -0,0 +1,5 @@
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("This is just a simple test of git, I want to test git push to a main branch instead of master branch");
+    }
+}
